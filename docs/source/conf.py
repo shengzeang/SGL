baee5f8f32d4c5a9b0dd21a3ab0e7d99b52f5c41
shengzeang <72069497+shengzeang@users.noreply.github.com>
2022-07-19 10:52:42 +0800

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,6 @@ from recommonmark.parser import CommonMarkParser
 from recommonmark.transform import AutoStructify
 
 
-sys.path.insert(0, os.path.abspath('./../../'))
 sys.path.insert(0, os.path.abspath('../../sgl/'))
 
 # Configuration file for the Sphinx documentation builder.
